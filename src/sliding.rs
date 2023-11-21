5ffb0e9eb43a49efc0e0c2350730016d5aea335c
Konrad Sosnowski <sosnowski.konrad@gmail.com>
2023-11-21 13:49:50 +1000

    wip

--- src/sliding.rs
+++ src/sliding.rs
@@ -1,6 +1,5 @@
 use async_stream::stream;
 use futures::StreamExt;
-use tokio::select;
 use tokio_stream::Stream;
 
 pub fn to_sliding_window<'a, T: Clone + 'a>(
@@ -9,18 +8,12 @@ pub fn to_sliding_window<'a, T: Clone + 'a>(
 ) -> impl Stream<Item = Vec<T>> + 'a {
     let mut buffer = Vec::with_capacity(window_size);
     stream! {
-        loop {
-            select! {
-                element = stream.next() => {
-                    let Some(element) = element else { break };
-
-                    buffer.push(element);
-                    if buffer.len() == window_size {
-                        yield buffer.clone();
-                        // slide down
-                        buffer.remove(0);
-                    }
-                }
+        while let Some(element) = stream.next().await {
+            buffer.push(element);
+            if buffer.len() == window_size {
+                yield buffer.clone();
+                // slide down
+                buffer.remove(0);
             }
         }
     }
--- src/tumbling.rs
+++ src/tumbling.rs
@@ -1,6 +1,5 @@
 use async_stream::stream;
 use futures::StreamExt;
-use tokio::select;
 use tokio_stream::Stream;
 
 pub fn to_tumbling_window<'a, T: Clone + 'a>(
@@ -9,16 +8,10 @@ pub fn to_tumbling_window<'a, T: Clone + 'a>(
 ) -> impl Stream<Item = Vec<T>> + 'a {
     let mut buffer = Vec::with_capacity(window_size);
     stream! {
-        loop {
-            select! {
-                element = stream.next() => {
-                    let Some(element) = element else { break };
-
-                    buffer.push(element);
-                    if buffer.len() == window_size {
-                        yield core::mem::take(&mut buffer);
-                    }
-                }
+        while let Some(element) = stream.next().await {
+            buffer.push(element);
+            if buffer.len() == window_size {
+                yield core::mem::take(&mut buffer);
             }
         }
     }

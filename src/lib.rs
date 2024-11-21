700eac5fee6695e6dca33bdec1eb228802e56755
Konrad Sosnowski <sosnowski.konrad@gmail.com>
2024-11-21 14:15:56 +1000

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,13 @@ use tokio_stream::Stream;
 pub mod merge;
 
 /// Tumbling window, waits till window of specified size is filled, emits, and starts a new window.
-pub fn to_tumbling_window<'a, T: 'a>(
-    mut stream: impl Stream<Item = T> + Unpin + 'a,
+pub fn to_tumbling_window<T, InStream>(
+    mut stream: InStream,
     window_size: usize,
-) -> impl Stream<Item = Vec<T>> + 'a {
+) -> impl Stream<Item = Vec<T>>
+where
+    InStream: Stream<Item = T> + Unpin,
+{
     let mut buffer = Vec::with_capacity(window_size);
     stream! {
         while let Some(element) = stream.next().await {
@@ -30,10 +33,14 @@ pub fn to_tumbling_window<'a, T: 'a>(
 }
 
 /// Sliding window, waits till window of specified size is filled, emits, and slides down by one element.
-pub fn to_sliding_window<'a, T: Clone + 'a>(
-    mut stream: impl Stream<Item = T> + Unpin + 'a,
+pub fn to_sliding_window<T, InStream>(
+    mut stream: InStream,
     window_size: usize,
-) -> impl Stream<Item = Vec<T>> + 'a {
+) -> impl Stream<Item = Vec<T>>
+where
+    T: Clone,
+    InStream: Stream<Item = T> + Unpin,
+{
     let mut buffer = Vec::with_capacity(window_size + 1);
     stream! {
         while let Some(element) = stream.next().await {
@@ -47,11 +54,16 @@ pub fn to_sliding_window<'a, T: Clone + 'a>(
 }
 
 /// Periodic window, waits till window of specified size is filled, emits, and starts a new window on a clock tick.
-pub fn to_periodic_window<'a, T: 'a, CT>(
-    mut stream: impl Stream<Item = T> + Unpin + 'a,
-    mut clock_stream: impl Stream<Item = CT> + Unpin + 'a,
+pub fn to_periodic_window<'a, T, CT, CTStream, InStream>(
+    mut stream: InStream,
+    mut clock_stream: CTStream,
     emit_last: bool,
-) -> impl Stream<Item = Vec<T>> + 'a {
+) -> impl Stream<Item = Vec<T>>
+where
+    T: 'a,
+    CTStream: Stream<Item = CT> + Unpin + 'a,
+    InStream: Stream<Item = T> + Unpin + 'a,
+{
     let mut buffer = vec![];
     stream! {
         loop {
@@ -79,15 +91,18 @@ pub fn to_periodic_window<'a, T: 'a, CT>(
 }
 
 /// Wrappers for actual window implementations, as an extension trait.
-pub trait WindowExt: Stream {
+pub trait WindowExt: Stream + Sized + Send
+where
+    Self::Item: Send,
+{
     fn tumbling_window<'a>(
         self,
         window_size: usize,
     ) -> Pin<Box<dyn Stream<Item = Vec<Self::Item>> + 'a>>
     where
-        Self: Unpin + Sized + 'a,
+        Self: Unpin + 'a,
     {
-        to_tumbling_window(self, window_size).boxed_local()
+        to_tumbling_window(self, window_size).boxed()
     }
 
     fn tumbling_window_unpin<'a>(
@@ -95,9 +110,9 @@ pub trait WindowExt: Stream {
         window_size: usize,
     ) -> Pin<Box<dyn Stream<Item = Vec<Self::Item>> + 'a>>
     where
-        Self: Sized + 'a,
+        Self: 'a,
     {
-        to_tumbling_window(Box::pin(self), window_size).boxed_local()
+        to_tumbling_window(Box::pin(self), window_size).boxed()
     }
 
     fn sliding_window<'a>(
@@ -105,10 +120,10 @@ pub trait WindowExt: Stream {
         window_size: usize,
     ) -> Pin<Box<dyn Stream<Item = Vec<Self::Item>> + 'a>>
     where
-        Self: Unpin + Sized + 'a,
+        Self: Unpin + 'a,
         Self::Item: Clone,
     {
-        to_sliding_window(self, window_size).boxed_local()
+        to_sliding_window(self, window_size).boxed()
     }
 
     fn sliding_window_unpin<'a>(
@@ -116,36 +131,45 @@ pub trait WindowExt: Stream {
         window_size: usize,
     ) -> Pin<Box<dyn Stream<Item = Vec<Self::Item>> + 'a>>
     where
-        Self: Sized + 'a,
+        Self: 'a,
         Self::Item: Clone,
     {
-        to_sliding_window(Box::pin(self), window_size).boxed_local()
+        to_sliding_window(Box::pin(self), window_size).boxed()
     }
 
-    fn periodic_window<'a, CT>(
+    fn periodic_window<'a, CT, CTStream>(
         self,
-        clock_stream: impl Stream<Item = CT> + Unpin + 'a,
+        clock_stream: CTStream,
         emit_last: bool,
     ) -> Pin<Box<dyn Stream<Item = Vec<Self::Item>> + 'a>>
     where
-        Self: Unpin + Sized + 'a,
+        CT: Send + 'a,
+        Self: Unpin + 'a,
+        CTStream: Stream<Item = CT> + Unpin + Send + 'a,
     {
-        to_periodic_window(self, clock_stream, emit_last).boxed_local()
+        to_periodic_window(self, clock_stream, emit_last).boxed()
     }
 
-    fn periodic_window_unpin<'a, CT>(
+    fn periodic_window_unpin<'a, CT, CTStream>(
         self,
-        clock_stream: impl Stream<Item = CT> + 'a,
+        clock_stream: CTStream,
         emit_last: bool,
     ) -> Pin<Box<dyn Stream<Item = Vec<Self::Item>> + 'a>>
     where
-        Self: Sized + 'a,
+        CT: Send + 'a,
+        Self: 'a,
+        CTStream: Stream<Item = CT> + Send + 'a,
     {
-        to_periodic_window(Box::pin(self), Box::pin(clock_stream), emit_last).boxed_local()
+        to_periodic_window(Box::pin(self), Box::pin(clock_stream), emit_last).boxed()
     }
 }
 
-impl<S> WindowExt for S where S: Stream {}
+impl<S> WindowExt for S
+where
+    S: Stream + Sized + Send,
+    S::Item: Send,
+{
+}
 
 #[cfg(test)]
 mod tests;
